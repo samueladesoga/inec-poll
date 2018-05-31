26cf72c52fad6344b7e49103cbddcbee9f8c2d29
Samuel Adesoga <samuel.adesoga@HMCTS.NET>
2018-05-31 08:35:09 +0100

    initialize method was wrongly spelt

--- scraper.rb
+++ scraper.rb
@@ -3,35 +3,43 @@ require 'Nokogiri'
 class Scraper
 
     attr_accessor :root_page
-    inec_base = "http://www.inecnigeria.org/?page_id=20"
-    lgass_url = "http://www.inecnigeria.org/wp-content/themes/inec/lgass.php"
-    ra_url = "http://www.inecnigeria.org/wp-content/themes/inec/ra.php"
+    INEC_BASE_URL = "http://www.inecnigeria.org/?page_id=20"
+    LAGASS_URL = "http://www.inecnigeria.org/wp-content/themes/inec/lgass.php"
+    RA_URL = "http://www.inecnigeria.org/wp-content/themes/inec/ra.php"
 
-	def initialise
-	   @doc = HTTParty.get(inec_base)
-	   @root_page = Nokogiri::HTML(doc)
+	def initialize
+	   @doc = HTTParty.get(INEC_BASE_URL)
+	   @root_page = Nokogiri::HTML(@doc)
 	end
 
 	def parse_states
-	   root_page.css("select#sel_states").css("option").map{|k|[k.values.first,k.text]}
+	   puts @root_page
+	   @root_page.css("select#sel_states").css("option").map{|k|[k.values.first,k.text]}
 	end
 
 	def get_lgas(state_id)
-		resp = post_to_url(lgass_url, :body => { :id => state_id}).parsed_response
-		Nokogiri::HTML(resp)
+		resp = post_to_url(LAGASS_URL, :body => { :id => state_id}).parsed_response
+		Nokogiri::HTML(resp).css("option").map{|k|[k.values.first,k.text]}
 	end
 
 	def get_ward(lga_id)
-		resp = post_to_url(ra_url, :body => { :id => lga_id}).parsed_response
-		Nokogiri::HTML(resp)
+		resp = post_to_url(RA_URL, :body => { :id => lga_id}).parsed_response
+		Nokogiri::HTML(resp).css("option").map{|k|[k.values.first,k.text]}
 	end
 
 	def get_polling_unit(state_id, lga_id, ward_id)
-		resp = post_to_url(inec_base, body = {:sel_states => state_id, :sel_lgas => lga_id, :sel_ra => ward_id, :MM_insert => 'form1'})
+		resp = post_to_url(INEC_BASE_URL, body = {:sel_states => state_id, :sel_lgas => lga_id, :sel_ra => ward_id, :MM_insert => 'form1'})
 	end
 
-	def post_to_url(url=lgass_url, body)
+	def post_to_url(url=LAGASS_URL, body)
 		HTTParty.post(url, body)
 	end
 
+	def main
+		parse_states.each { |value, state_name|
+			get_lgas(value.to_i).each { |lg_v, lg_name|
+				puts get_ward(lg_v.to_i)
+			}
+		}
+	end
 end
